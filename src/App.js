59ca18a9bd39b80025de5711282a1ad93878fc97
MNM <moussanm2014@gmail.com>
2021-03-22 20:47:25 -0500

    modified src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import NotFound from "./pages/404";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Portfolio</h1>
+    <div className="container">
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/Projects" component={ Projects } />
+          <Route exact path="/Contact" component={ Contact } />
+          <Route component={ NotFound }/>
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
