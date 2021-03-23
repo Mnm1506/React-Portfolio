1a6429c437b50c9628e28de0c2f6a64bdf4ad887
MNM <moussanm2014@gmail.com>
2021-03-23 08:54:58 -0500

    gh pages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import NotFound from "./pages/404";
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="container">
       <Navbar />
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/Projects" component={ Projects } />
