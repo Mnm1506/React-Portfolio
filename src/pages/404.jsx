42ad0d9adf1b6f4960c5f381e57f02d1be15b6cb
MNM <moussanm2014@gmail.com>
2021-03-22 21:36:56 -0500

    gh pages deployment

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+// import React from "react";
 
-const NotFound = () => {
-    return(
-        <div>
-            <h2>Error Page not found!</h2>
-        </div>
-    )
-}
+// const NotFound = () => {
+//     return(
+//         <div>
+//             <h2>Error Page not found!</h2>
+//         </div>
+//     )
+// }
 
-export default NotFound;
+// export default NotFound;
