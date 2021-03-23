f3d149f970bb5fd4ec288077b17a33bbd02ea70b
MNM <moussanm2014@gmail.com>
2021-03-23 12:04:42 -0500

    modified app.js file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import Header from "../components/Header";
-import Projects from "../components/Projects"
+import Projects from "../components/Projects";
+import Navbar from "./components/Navbar";
 
 const Home = () => {
     const [project] = useState([    ])
     return(
         <div>
+            <Navbar />
             <Header />
             <Projects project={project} />
            
