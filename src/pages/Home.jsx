b5b063c62a84e9cc576aa3e13f16254b06eb4d00
MNM <moussanm2014@gmail.com>
2021-03-23 22:42:41 -0500

    gh pages deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import Header from "../components/Header";
-import Projects from "../components/Projects"
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
 
 const Home = () => {
     const [project] = useState([    ])
     return(
         <div>
             <Header />
+            <Navbar />
             <Projects project={project} />
            
         </div>
